85163a50b7576cab4c9f23b3833c0935e397e86c
oussamajamil <55845632+oussamajamil@users.noreply.github.com>
2023-12-03 19:26:34 +0100

    if user connect

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -2,13 +2,17 @@ import { db } from "@/db";
 import React from "react";
 import { ProductsTable } from "./_components/product-table";
 import AddProduct from "./_components/add-product";
+import getCurrentUser from "@/actions/get-current-user";
 
 export default async function Home() {
   const products = await db.product.findMany();
+  const user = await getCurrentUser();
   return (
     <div className="p-6 flex flex-col gap-4">
       <div className="flex justify-end">
-        <AddProduct />
+        {
+          user &&   <AddProduct />
+        }
       </div>
       <ProductsTable products={products} />
     </div>
